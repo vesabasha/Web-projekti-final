884e94f519b585896b5ae9b5e10a7c1969dd1114
er:)ind <lowfione2@gmail.com>
2026-01-25 17:14:22 +0100

    Config and pdo

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// basically the communication line to the database
+//e bona gen most of it eshte boilerplate code
+$host = 'localhost';
+$dbname = 'quest_db';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
