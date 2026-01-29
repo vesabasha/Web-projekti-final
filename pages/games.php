34cd6c9104b761aca14697fa60fd9784894345b4
er:)ind <lowfione2@gmail.com>
2026-01-29 16:44:01 +0100

    changed games.html into games.php
    i LOVE DEVON HENDRYX! :)

---
+++ pages/games.php
@@ -0,0 +1,250 @@
+<?php
+session_start();
+$games_file = __DIR__ . '/../games.json';
+$games = [];
+if (file_exists($games_file)) {
+    $json_content = file_get_contents($games_file);
+    $games = json_decode($json_content, true);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    
+
+
+</head>
+<body>
+
+<!--testing per commit, erind balaj-->
+    <!--navigation bar, e boni copy paste qeta ncdo faqe ever -->
+    <nav>
+        <ul>
+            <li><img href="#" style="width: 50px; height: 50px; cursor: pointer;" src="images/logo.jpg" alt="Logo"></li>
+            <li><a style="font-size:30px" id="title" href="landing">QUEST</a></li>
+
+            <li style="margin-left: auto;"><a  class="navitem" href="games">Browse Games</a></li>
+            <li><a class="navitem" href="privacy">Our Policy</a></li>
+
+            <li><input type="search" placeholder=" Search..."></li>
+
+            <li><button id="loginBtn">Log in</button></li>
+            <li><button id="signupBtn" class="button-2">Sign Up</button></li>
+        </ul>
+    </nav>
+    <!-- login and signup -->
+     <!-- login -->
+    <div id="loginModal" class="modal hidden">
+        <div class="modal-content">
+            <button class="modal-close">&times;</button>
+            <h2>Log in</h2>
+
+            <button class="google-btn">
+                <img src="images/googleicon.png" alt="Google Icon">
+                Continue with Google
+            </button>
+
+            <p class="divider-text">or</p>
+
+            <form class="modal-form">
+                <label>
+                    Email
+                    <input type="email" placeholder="Enter your email or username">
+                </label>
+                <label>
+                    Password
+                    <input type="password" placeholder="Enter your password">
+                </label>
+
+                <div style="margin-bottom: 12px;"></div>
+
+                <button type="button" onclick="window.location.href='profile.html'" >Log in</button>
+            </form>
+
+                <p style="color:white; margin-top:15px; text-align:center; font-size:14px;">
+                <button id="reset" style="
+                    background:none;
+                    border:none;
+                    color:#44A1A0;
+                    cursor:pointer;
+                    font-size:14px;
+                    text-decoration:underline;
+                ">Reset password</button>
+
+                <p style="color:white; margin-top:15px; text-align:center; font-size:14px;">
+                No account?
+                <button id="swapToSignup" style="
+                    background:none;
+                    border:none;
+                    color:#44A1A0;
+                    cursor:pointer;
+                    font-size:14px;
+                    text-decoration:underline;
+                ">Create one</button>
+            </p>
+        </div>
+    </div>
+
+    <!-- signup -->
+    <div id="signupModal" class="modal hidden">
+        <div class="modal-content">
+            <button class="modal-close">&times;</button>
+            <h2>Sign Up</h2>
+
+            <button class="google-btn">
+                <img src="images/googleicon.png" alt="Google Icon">
+                Continue with Google
+            </button>
+
+            <p class="divider-text">or</p>
+
+            <form class="modal-form">
+                <label>
+                    Username
+                    <input type="text" placeholder="Choose a username" >
+                </label>
+
+                <label>
+                    Email
+                    <input type="email" placeholder="Enter your email" >
+                </label>
+
+                <label>
+                    Age
+                    <input type="number" min="1" placeholder="Your age" >
+                </label>
+
+                <label>
+                    Phone Number
+                    <input type="tel" placeholder="+383 44 123 456" >
+                </label>
+
+                <label>
+                    Password
+                    <input type="password" placeholder="Create a password" >
+                </label>
+
+                <label>
+                    Confirm Password
+                    <input type="password" placeholder="Confirm your password" >
+                </label>
+
+                <div style="margin-bottom: 12px;"></div>
+
+                <button type="button" onclick="window.location.href='profile.html'" class="button-2">Create Account</button>
+            </form>
+
+            <p style="color:white; margin-top:15px; text-align:center; font-size:14px;">
+                Already a member?
+                <button id="swapToLogin" style="
+                    background:none;
+                    border:none;
+                    color:#44A1A0;
+                    cursor:pointer;
+                    font-size:14px;
+                    text-decoration:underline;
+                ">Log in!</button>
+            </p>
+        </div>
+    </div>
+
+<!--header -->
+
+<div style="margin: 40px auto; width: 75%; display: flex; flex-direction: row; margin-bottom: 50px; align-items: center;">
+    <h1 class="h1-title" style="flex-grow: 0.5; margin: 0;">Find the perfect game for you</h1>
+    <input style="height:45px; width:42%; margin-left: 5%;" type="search" placeholder=" Search...">
+</div>
+
+<!-- qetu i qesim filters advanced search -->
+<div style="width: 75%; margin: 0 auto;">
+    <button id="advancedBtn" style="height: auto; width: 100%; cursor: pointer; border: none; border-radius: 8px 8px 0 0; background-color: #010B14; color: white; text-align: left;">
+        <p style="padding-left: 2%; font-size: 20px;" class="secondary-text">Advanced Search</p>
+    </button>
+
+    <div id="advancedBox" style="display: none; width: 100%; background-color: #010B14; color: white; border-top: 1px solid #000; border-radius: 0 0 8px 8px;">
+        <p class="secondary-text" style="padding-left: 2%;">Seems like you're early...</p>
+        <p class="secondary-text" style="padding-left:  2%; padding-bottom:2%;">Come back in phase two of the project to be able to filter by genre, date etc. from more games, as well as use our AI feature.</p>
+    </div>
+</div>
+
+<!-- games  -->
+ <div style="margin-top: 2%;" class="game-container" id="game-container"></div>
+
+
+   <footer>
+        <div class="footer-columns">
+            <div class="col">
+            <h2>QUEST</h2>
+            </div>
+            <div class="col">
+            <a href="#">About Us</a><br>
+            <a href="#">Contact Us</a>
+            </div>
+            <div class="col">
+            <a href="#">Terms of Service</a><br>
+            <a href="#">Privacy Policy</a>
+            </div>
+        </div>
+        <br>
+        <div class="footer-bottom">
+            © 2025 Quest. All rights reserved. Game data and artwork belong to their respective owners.
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+
+    <script>
+    fetch('games.json')
+        .then(response => response.json())
+        .then(games => {
+            const container = document.getElementById("game-container");
+
+            games.forEach(game => {
+                const card = document.createElement("div");
+                card.className = "game-card";
+                const imgSrc = game.main_image_url || "images/placeholder.png";
+                card.innerHTML = `
+                    <img src="${game.main_image_url}" alt="${game.name}" onerror="this.src='images/games/placeholder.png';">
+                    <div class="game-card-info">
+                        <h3>${game.name}</h3>
+                        <p style="display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;" class="secondary-text">${game.description}</p>
+                        <div class="genre-container">
+                            ${game.genres.map(genre => `<p class="genre-badge">${genre}</p>`).join('')}
+                        </div>
+                    </div>
+                `;
+
+                card.addEventListener('click', () => {
+                window.location.href = `details.html?game=${encodeURIComponent(game.name)}`;
+            });
+
+                container.appendChild(card);
+            });
+        })
+        .catch(error => console.error("Error loading games.json:", error));
+
+
+
+    const btn = document.getElementById('advancedBtn');
+    const box = document.getElementById('advancedBox');
+
+    btn.addEventListener('click', () => {
+        // toggle display
+        if (box.style.display === 'none') {
+            box.style.display = 'block';
+        } else {
+            box.style.display = 'none';
+        }
+    });
+    </script>
+    
+</html>
