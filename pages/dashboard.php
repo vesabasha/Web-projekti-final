ecef19b45c980d64a980ff9b131e6c3053105caf
Vesab <vesa@asaasin.ai>
2026-02-02 18:08:23 +0100

    small fix

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -44,6 +44,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="styledashboard.css">
+  <link rel="stylesheet" href="../list.css">
 </head>
 <body>
 
